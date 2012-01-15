f12d9054e9049c225b70fcb2e2106d001af53a0e
Jelmer <jelmer@ikhoefgeen.nl>
2012-01-15 10:21:44 +0100

    Now [download] will actually offer the file for download.

--- www/download.php
+++ www/download.php
@@ -3,5 +3,6 @@
 if (isset($_GET['kb']) && preg_match('/^[a-zA-Z0-9_\-\.]+\.xml$/i', $_GET['kb']))
 {
 	header('Content-Type: text/xml');
+	header('Content-Disposition: attachment; filename=' . $_GET['kb']);
 	readfile('../knowledgebases/' . $_GET['kb']);
 }
