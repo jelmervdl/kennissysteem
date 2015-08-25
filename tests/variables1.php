89f6d3e01722b1db8aba2f289e2a8b3e80e9ee91
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-08-25 14:22:55 +0200

    Added two test cases for behaviour of variables

---
+++ tests/variables1.php
@@ -0,0 +1,26 @@
+<?php
+
+require '../util.php';
+require '../solver.php';
+
+$test_description = "Testing a variable in the fact value side in the knowledege state facts dict.";
+
+$rule = new Rule();
+$rule->condition = new FactCondition('a', 'c');
+$rule->consequences = ['test' => 'passed'];
+$rule->inferred_facts->push('test');
+
+$state = new KnowledgeState();
+$state->facts = [
+	'a' => '$b',
+	'b' => 'c'
+];
+$state->rules->push($rule);
+$state->goalStack->push('test');
+
+$solver = new Solver();
+
+$solver->solveAll($state);
+
+// The fact that c is true should have been recorded
+assert('$state->value("test") == "passed"');
---
+++ tests/variables2.php
@@ -0,0 +1,27 @@
+<?php
+
+require '../util.php';
+require '../solver.php';
+
+$test_description = "Testing a variable in the fact name side of a fact condition.";
+
+$rule = new Rule();
+$rule->condition = new FactCondition('$a', 'ok');
+$rule->consequences = ['test' => 'passed'];
+$rule->inferred_facts->push('test');
+
+$state = new KnowledgeState();
+$state->facts = [
+	'a' => '$b',
+	'b' => 'c',
+	'c' => 'ok'
+];
+$state->rules->push($rule);
+$state->goalStack->push('test');
+
+$solver = new Solver();
+
+$solver->solveAll($state);
+
+// The fact that c is true should have been recorded
+assert('$state->value("test") == "passed"');
