571619d57dd1110296f958bc5e2a1006ba5b41a0
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-11-19 11:52:34 +0100

    We've got a function for that

--- www/webfrontend.php
+++ www/webfrontend.php
@@ -126,11 +126,10 @@ class WebFrontend
 			{
 				$state->goalStack->push($goal->name);
 
-				// Also push any answer values that are variables as goals
-				// to be solved.
+				// Also push any answer values that are variables as goals to be solved.
 				foreach ($goal->answers as $answer)
 					if (KnowledgeState::is_variable($answer->value))
-						$state->goalStack->push(substr($answer->value, 1));	
+						$state->goalStack->push(KnowledgeState::variable_name($answer->value));	
 			}
 
 		return $state;
